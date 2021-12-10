b6046ee94ce2b2e5b631c587ac4a4b66e56f8f0b
forcemain <forcemain@163.com>
2021-12-10 16:05:49 +0800

    :underage:

--- project/router/login.py
+++ project/router/login.py
@@ -9,6 +9,7 @@ import typing as t
 
 from logging import getLogger
 from project.service import Service
+from itsdangerous.exc import BadData
 from project.models import OAuth2UserModel
 from service_core.core.as_router import ApiRouter
 from service_webserver.core.request import Request
@@ -39,6 +40,17 @@ class OAuth2Login(Endpoint):
         """
         url_base64 = request.args.get('next')
         return base64.b64decode(url_base64.encode('utf-8')).decode('utf-8')
+
+    @staticmethod
+    def get_session_id(service: Service, request: Request) -> t.Union[t.Text, None]:
+        """ 从请求对象中获取会话id
+
+        @param service: 服务对象
+        @param request: 请求对象
+        @return: t.Text
+        """
+        session_id = request.cookies.get('session_id', default='')
+        return service.cookie_serializer.loads(session_id)
 
     @staticmethod
     def set_session_id(service: Service, response: Response, user_id: int) -> None:
@@ -78,11 +90,16 @@ class OAuth2Login(Endpoint):
         @return: Response
         """
         next_url = self.get_next_url(request)
-        username = request.form.get('username')
-        password = request.form.get('password')
-        # 此处请自行添加帐号密码验证逻辑,具体场景具体分析
-        with safe_transaction(service.orm, commit=False) as session:
-            user = session.query(OAuth2UserModel).filter(OAuth2UserModel.name == username).first()
-        response = RedirectResponse(next_url)
-        self.set_session_id(service, response=response, user_id=user.id)
+        try:
+            self.get_session_id(service, request=request)
+            response = RedirectResponse(next_url)
+        except BadData:
+            logger.error(f'unexpected error while unserializer session_id', exc_info=True)
+            username = request.form.get('username')
+            password = request.form.get('password')
+            # 此处请自行添加帐号密码验证逻辑,具体场景具体分析
+            with safe_transaction(service.orm, commit=False) as session:
+                user = session.query(OAuth2UserModel).filter(OAuth2UserModel.name == username).first()
+            response = RedirectResponse(next_url)
+            self.set_session_id(service, response=response, user_id=user.id)
         return response
